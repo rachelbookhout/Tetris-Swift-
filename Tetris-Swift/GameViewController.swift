4eda910e30a1b1f4f862b95874191507b5ee0184
rachelbookhout <rbookhou@gmail.com>
2015-02-18 19:13:40 -0500

    next

--- Tetris-Swift/GameViewController.swift
+++ Tetris-Swift/GameViewController.swift
@@ -12,9 +12,18 @@ import SpriteKit
 
 
 class GameViewController: UIViewController {
-
+ var scene: GameScene!
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()     // Configure the view.
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        // Present the scene.
+        skView.presentScene(scene)
 
     
         }
