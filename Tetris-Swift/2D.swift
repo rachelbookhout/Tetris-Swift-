4cf61d398ab1ac44f3e68cb19c22d1dd9f08e63e
rachelbookhout <rbookhou@gmail.com>
2015-02-20 22:00:15 -0500

    added class

--- Tetris-Swift/2D.swift
+++ Tetris-Swift/2D.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    init (columns: Int, rows: Int){
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
+
+
