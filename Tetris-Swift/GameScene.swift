70a65b6ebce00fe5399a5e293d98f7452a5a86a1
rachelbookhout <rbookhou@gmail.com>
2015-02-22 17:28:57 -0500

    added seconds

--- Tetris-Swift/GameScene.swift
+++ Tetris-Swift/GameScene.swift
@@ -8,7 +8,16 @@
 
 import SpriteKit
 
+// #1
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
+    
+    // #2
+    var tick:(() -> ())?
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:NSDate?
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoder not supported")
     }
@@ -23,9 +32,27 @@ class GameScene: SKScene {
         background.anchorPoint = CGPoint(x: 0, y: 1.0)
         addChild(background)
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        // #3
+        if lastTick == nil {
+            return
+        }
+        var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        if timePassed > tickLengthMillis {
+            lastTick = NSDate()
+            tick?()
+        }
+    }
+    
+    // #4
+    func startTicking() {
+        lastTick = NSDate()
+    }
+    
+    func stopTicking() {
+        lastTick = nil
     }
 }
 
