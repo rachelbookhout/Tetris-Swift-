9846034e34c5ffdb7ae425927b1f625389cf9bd5
rachelbookhout <rbookhou@gmail.com>
2015-02-26 20:56:40 -0500

    added class

--- Tetris-Swift/shape.swift
+++ Tetris-Swift/shape.swift
@@ -6,4 +6,38 @@
 //  Copyright (c) 2015 Rachel Bookhout. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+let NumOrientations: UInt32 = 4
+
+enum Orientation: Int, Printable {
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String {
+        switch self {
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+        }
+    }
+    
+    static func random() -> Orientation {
+        return Orientation(rawValue:Int(arc4random_uniform(NumOrientations)))!
+    }
+    
+    // #1
+    static func rotate(orientation:Orientation, clockwise: Bool) -> Orientation {
+        var rotated = orientation.rawValue + (clockwise ? 1 : -1)
+        if rotated > Orientation.TwoSeventy.rawValue {
+            rotated = Orientation.Zero.rawValue
+        } else if rotated < 0 {
+            rotated = Orientation.TwoSeventy.rawValue
+        }
+        return Orientation(rawValue:rotated)!
+    }
+}
