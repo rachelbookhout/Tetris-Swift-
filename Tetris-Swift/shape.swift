ef17c0fef90f36d1a706aebd716a031c398e5182
rachelbookhout <rbookhou@gmail.com>
2015-03-02 06:33:50 -0500

    added

--- Tetris-Swift/shape.swift
+++ Tetris-Swift/shape.swift
@@ -12,7 +12,7 @@ let NumOrientations: UInt32 = 4
 
 enum Orientation: Int, Printable {
     case Zero = 0, Ninety, OneEighty, TwoSeventy
-    
+
     var description: String {
         switch self {
         case .Zero:
@@ -25,11 +25,11 @@ enum Orientation: Int, Printable {
             return "270"
         }
     }
-    
+
     static func random() -> Orientation {
         return Orientation(rawValue:Int(arc4random_uniform(NumOrientations)))!
     }
-    
+
     // #1
     static func rotate(orientation:Orientation, clockwise: Bool) -> Orientation {
         var rotated = orientation.rawValue + (clockwise ? 1 : -1)
@@ -52,14 +52,14 @@ let FourthBlockIdx: Int = 3
 class Shape: Hashable, Printable {
     // The color of the shape
     let color:BlockColor
-    
+
     // The blocks comprising the shape
     var blocks = Array<Block>()
     // The current orientation of the shape
     var orientation: Orientation
     // The column and row representing the shape's anchor point
     var column, row:Int
-    
+
     // Required Overrides
     // #1
     // Subclasses must override this property
@@ -78,18 +78,18 @@ class Shape: Hashable, Printable {
         }
         return []
     }
-    
+
     // Hashable
     var hashValue:Int {
         // #4
         return reduce(blocks, 0) { $0.hashValue ^ $1.hashValue }
     }
-    
+
     // Printable
     var description:String {
         return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
     }
-    
+
     init(column:Int, row:Int, color: BlockColor, orientation:Orientation) {
         self.color = color
         self.column = column
@@ -97,12 +97,24 @@ class Shape: Hashable, Printable {
         self.orientation = orientation
         initializeBlocks()
     }
-    
+
     // #5
     convenience init(column:Int, row:Int) {
         self.init(column:column, row:row, color:BlockColor.random(), orientation:Orientation.random())
     }
 }
+
+final func initializeBlocks() {
+// #2
+         if let blockRowColumnTranslations = blockRowColumnPositions[orientation] {
+             for i in 0..<blockRowColumnTranslations.count {
+                 let blockRow = row + blockRowColumnTranslations[i].rowDiff
+                 let blockColumn = column + blockRowColumnTranslations[i].columnDiff
+                 let newBlock = Block(column: blockColumn, row: blockRow, color: color)
+                 blocks.append(newBlock)
+             }
+         }
+     }
 
 func ==(lhs: Shape, rhs: Shape) -> Bool {
     return lhs.row == rhs.row && lhs.column == rhs.column
